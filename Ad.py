6dd5d7ebb3db155ad72e96f9263f194a7113c689
Halvor Reiten <ha.reiten@gmail.com>
2020-05-28 22:03:00 +0200

    Fewer API requests, batch_update replaces single update for each, and added a new field to keep_columns

--- Ad.py
+++ Ad.py
@@ -118,8 +118,18 @@ class Ad:
     def to_link(self, to, text):
         return '=HYPERLINK(\"{}\";\"{}\")'.format(to, text)
 
-    # the keys in this dict must correspond to the headers in the worksheet, but order is not important.
-    def get_values_dict(self):
+    def get_row_in_sheet(self, addresses=None, worksheet=None):
+        if addresses is None:
+            if worksheet is None:
+                raise Exception("No worksheet supplied")
+            addresses = worksheet.col_values(1)[1:]
+
+        try:
+            return addresses.index(self.adresse) + 2
+        except:
+            return -1
+
+    def get_ad_dict(self):
         return {
             'Adresse': self.to_link(self.uri, self.adresse),
             'Område': self.område,
@@ -141,42 +151,50 @@ class Ad:
             'Status': self.status
         }
 
-    def push_to_worksheet(self, worksheet, arg_dict={}):
-        insert_at_row = len(worksheet.col_values(1)) + 1
-        headers = worksheet.row_values(1)
-        d = dict.fromkeys(headers)
-        ad_dict = self.get_values_dict()
+    def get_sheet_dict(self, worksheet, headers=None, addresses=None, arg_dict={}):
+        ad_dict = self.get_ad_dict()
+
+        if headers is None:
+            headers = worksheet.row_values(1)
+
+        sheet_dict = dict.fromkeys(headers)
 
         # populate the dictionary
-        for header in d.keys():
-            d[header] = safe_get_from_dict(ad_dict, header)
+        for key in sheet_dict:
+            sheet_dict[key] = safe_get_from_dict(ad_dict, key)
 
         # keep "manual" data, e.g. user comments, that is not scraped
-        addresses = worksheet.col_values(1)[1:]
-        ad_already_in_sheet = self.adresse in addresses
-        if ad_already_in_sheet:
-            ad_row = addresses.index(self.adresse) + 2
-            keep_columns = ["Vurdering", "Max bud",
-                            "Kommentar", "Påmeldt budrunde", "Lagt til"]
+        ad_row = self.get_row_in_sheet(addresses, worksheet)
+        if ad_row >= 0:
+            ad_data = worksheet.row_values(ad_row)
+            keep_columns = ["Vurdering",
+                            "Max bud",
+                            "Tregulv + Takhøyde + Dobbeltdører",
+                            "Kommentar",
+                            "Påmeldt budrunde",
+                            "Lagt til"]
+
             for col_name in keep_columns:
                 if col_name in headers:
-                    col_value = worksheet.cell(
-                        ad_row, headers.index(col_name)+1).value
-                    d[col_name] = col_value
+                    sheet_dict[col_name] = ad_data[headers.index(col_name)]
 
-            if len(arg_dict) > 0:
-                for key, value in arg_dict.items():
-                    if key in d.keys() and value:
-                        d[key] = value
-
-            # delete the row
-            worksheet.delete_rows(ad_row)
         else:
-            d["Lagt til"] = datetime.datetime.now().strftime("%d.%m.%Y")
+            sheet_dict["Lagt til"] = datetime.datetime.now().strftime(
+                "%d.%m.%Y")
 
+        if len(arg_dict) > 0:
             for key, value in arg_dict.items():
-                if key in d.keys() and value:
-                    d[key] = value
+                if key in sheet_dict and value:
+                    sheet_dict[key] = value
 
-        # push to google worksheet
-        return worksheet.append_row(list(d.values()), "USER_ENTERED")
+        return sheet_dict
+
+    def push_to_worksheet(self, worksheet, headers=None, addresses=None, arg_dict={}):
+        sheet_dict = self.get_sheet_dict(
+            worksheet=worksheet, headers=headers, addresses=addresses, arg_dict=arg_dict)
+        ad_row = self.get_row_in_sheet(addresses, worksheet)
+
+        if ad_row >= 0:
+            return worksheet.update(f"{ad_row}:{ad_row}", [list(sheet_dict.values())], value_input_option="USER_ENTERED")
+
+        return worksheet.append_row(list(sheet_dict.values()), value_input_option="USER_ENTERED")
--- main.py
+++ main.py
@@ -25,66 +25,88 @@ def ping_uri(uri, verbose=0):
         vprint(f"✔ The URI responded with status code {ping}.", verbose)
 
 
+def get_index(array, *argv):
+    for arg in argv:
+        if arg in array:
+            return arg, array.index(arg)
+    return None, -1
+
+
+def exists_flag_argument(flag, return_flag_value=False):
+    found_flag, found_idx = get_index(sys.argv, flag, f'-{flag}', f'--{flag}')
+    flag_exists = found_flag is not None
+
+    def get_value(
+        args, flag): return args[found_idx+1] if flag_exists else None
+
+    return flag_exists if not return_flag_value else get_value(sys.argv, flag)
+
+
 def update_existing_records(verbose=0):
     client = authenticate(json_path=GOOGLE_CREDENTIALS_JSON_PATH)
     worksheet = client.open(
         GOOGLE_SPREADSHEET_NAME).get_worksheet(WORKSHEET_INDEX)
+    headers = worksheet.row_values(1)
+    addresses = worksheet.col_values(1)[1:]
 
     def fmt_link(link): return link.split("\";")[0].split("=HYPERLINK(\"")[-1]
 
     links = [fmt_link(ad[0]) for ad in worksheet.get_all_values(
         value_render_option="FORMULA")[1:] if len(ad[0]) > 0]
 
-    for finn_uri in tqdm(links, unit="ad"):
-        ad = Ad(finn_uri)
-        ad.push_to_worksheet(worksheet)
+    vprint("Scraping ad information...", verbose)
 
+    dicts = []
+    for i, link in enumerate(tqdm(links, ascii=True, unit="ad")):
+        ad = Ad(link)
+        sheet_dict = {
+            "range": f"{i+2}:{i+2}",
+            "values": [list(ad.get_sheet_dict(
+                worksheet, headers, addresses).values())]
+        }
+        dicts.append(sheet_dict)
 
-def any_match(container_list, *argv):
-    return any([arg in container_list for arg in argv])
+    vprint(f"Writing ${len(dicts)} to google sheets...", verbose)
+    return worksheet.batch_update(dicts, value_input_option="USER_ENTERED")
 
 
 def run():
-    verbose = 0 if any_match(sys.argv, "--silent", "-silent") else 1
+    no_google_flag = exists_flag_argument("no-google")
+    should_update_flag = exists_flag_argument("update")
+    silent_flag = exists_flag_argument("silent")
+    verbose = 0 if silent_flag else 1
+    uri = exists_flag_argument("uri", True)
 
-    if any_match(sys.argv, "--update", "-update"):
+    if should_update_flag:
         confirm = input(
             "> Are you sure you wish to update all records in the spreadsheet? [yes | no] ")
-        if confirm.lower() == "yes" or confirm.lower() == "y":
-            update_existing_records(verbose=0)
-        else:
-            print("Aborting...")
+
+        if confirm.lower() in ["yes", "y"]:
+            update_existing_records(verbose=verbose)
         return
 
-    uri = ""
-    if any_match(sys.argv, "--uri", "-uri"):
-        uri = sys.argv[sys.argv.index(
-            "--uri" if "--uri" in sys.argv else "-uri")+1]
-    else:
+    if uri is None:
         uri = input("> Finn URI: ")
 
     ping_uri(uri, verbose)
     ad = Ad(uri)
 
-    if any_match(sys.argv, "--no-google", "-no-google"):
+    if no_google_flag:
         print(f"Data for URI {uri}")
-        pprint(ad.get_values_dict())
+        pprint(ad.get_ad_dict())
         return
 
-    arg_dict = dict.fromkeys(["Kommentar", "Vurdering"])
-    if any_match(sys.argv, "-k", "--k"):
-        arg_dict["Kommentar"] = sys.argv[sys.argv.index(
-            "--k" if "--k" in sys.argv else "-k")+1]
-
-    if any_match(sys.argv, "-v", "--v"):
-        arg_dict["Vurdering"] = sys.argv[sys.argv.index(
-            "--v" if "--v" in sys.argv else "-v")+1]
+    arg_dict = {}
+    arg_dict["Vurdering"] = exists_flag_argument("v", True)
+    arg_dict["Kommentar"] = exists_flag_argument("k", True)
+    arg_dict["Tregulv + Takhøyde + Dobbeltdører"] = exists_flag_argument(
+        "t", True)
 
     client = authenticate(json_path=GOOGLE_CREDENTIALS_JSON_PATH)
     worksheet = client.open(
         GOOGLE_SPREADSHEET_NAME).get_worksheet(WORKSHEET_INDEX)
 
-    ad.push_to_worksheet(worksheet, arg_dict)
+    ad.push_to_worksheet(worksheet, arg_dict=arg_dict)
     vprint(
         f"✔ Success! Ad \"{ad.adresse}\" pushed to the spreadsheet.", verbose)
 
